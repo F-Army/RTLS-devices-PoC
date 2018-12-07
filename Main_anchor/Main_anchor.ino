15287139a312f93e7434c4e045e3ab23cff7c8d4
Michele Biondi <michelebiondi01@gmail.com>
2018-12-07 17:05:39 +0100

    Add connection to server for main anchor

--- Main_anchor/Main_anchor.ino
+++ Main_anchor/Main_anchor.ino
@@ -10,6 +10,8 @@
 #include <DW1000NgUtils.hpp>
 #include <DW1000NgRanging.hpp>
 #include <DW1000NgRTLS.hpp>
+#include <ESP8266HTTPClient.h>
+#include "config.h"
 
 typedef struct Position {
     double x;
@@ -37,6 +39,10 @@ boolean received_B = false;
 
 byte target_eui[8];
 byte tag_shortAddress[] = {0x05, 0x00};
+
+uint16_t tagAddress = 5;
+
+uint16_t this_anchor = 1;
 
 byte anchor_b[] = {0x02, 0x00};
 uint16_t next_anchor = 2;
@@ -71,7 +77,7 @@ void setup() {
     // DEBUG monitoring
     Serial.begin(115200);
     Serial.println();
-    WiFi.begin("", "");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     Serial.print("Connecting");
     while (WiFi.status() != WL_CONNECTED)
@@ -102,7 +108,7 @@ void setup() {
     DW1000Ng::setReceiveFrameWaitTimeoutPeriod(5000);
 
     DW1000Ng::setNetworkId(RTLS_APP_ID);
-    DW1000Ng::setDeviceAddress(1);
+    DW1000Ng::setDeviceAddress(this_anchor);
 	
     DW1000Ng::setAntennaDelay(16436);
     
@@ -117,21 +123,6 @@ void setup() {
     Serial.print("Network ID & Device Address: "); Serial.println(msg);
     DW1000Ng::getPrintableDeviceMode(msg);
     Serial.print("Device mode: "); Serial.println(msg);    
-}
-
-/* using https://math.stackexchange.com/questions/884807/find-x-location-using-3-known-x-y-location-using-trilateration */
-void calculatePosition(double &x, double &y) {
-
-    /* This gives for granted that the z plane is the same for anchor and tags */
-    double A = ( (-2*position_self.x) + (2*position_B.x) );
-    double B = ( (-2*position_self.y) + (2*position_B.y) );
-    double C = (range_self*range_self) - (range_B*range_B) - (position_self.x*position_self.x) + (position_B.x*position_B.x) - (position_self.y*position_self.y) + (position_B.y*position_B.y);
-    double D = ( (-2*position_B.x) + (2*position_C.x) );
-    double E = ( (-2*position_B.y) + (2*position_C.y) );
-    double F = (range_B*range_B) - (range_C*range_C) - (position_B.x*position_B.x) + (position_C.x*position_C.x) - (position_B.y*position_B.y) + (position_C.y*position_C.y);
-
-    x = (C*E-F*B) / (E*A-B*D);
-    y = (C*D-A*F) / (B*D-A*E);
 }
 
 void loop() {
@@ -149,30 +140,25 @@ void loop() {
             if(!result.success) return;
             range_self = result.range;
 
-            String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
-            rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
-            Serial.println(rangeString);
-
-        } else if(recv_data[9] == 0x60) {
-            double range = static_cast<double>(DW1000NgUtils::bytesAsValue(&recv_data[10],2) / 1000.0);
-            String rangeReportString = "Range from: "; rangeReportString += recv_data[7];
-            rangeReportString += " = "; rangeReportString += range;
-            Serial.println(rangeReportString);
-            if(received_B == false && recv_data[7] == anchor_b[0] && recv_data[8] == anchor_b[1]) {
-                range_B = range;
-                received_B = true;
-            } else if(received_B == true && recv_data[7] == anchor_c[0] && recv_data[8] == anchor_c[1]){
-                range_C = range;
-                double x,y;
-                calculatePosition(x,y);
-                String positioning = "Found position - x: ";
-                positioning += x; positioning +=" y: ";
-                positioning += y;
-                Serial.println(positioning);
-                received_B = false;
-            } else {
-                received_B = false;
-            }
+            // Call server
+            HTTPClient http;
+            String request = "";
+                request += "anchor=";
+                request += this_anchor;
+                request += "&tag=";
+                request += tagAddress;
+                request += "&range=";
+                request += range_self;
+            http.begin(SERVER_URL);
+            http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+            int httpCode = http.POST(request);
+            String payload = http.getString();
+            Serial.print("Range sent: ");
+            Serial.print(httpCode);
+            Serial.print(" ");
+            Serial.print(payload);
+            Serial.println();
+            http.end();
         }
     }
 
